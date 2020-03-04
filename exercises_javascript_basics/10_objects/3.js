3a3a80a868f4d78013105ba51a92708994bc64c9
bryansteele <bryan@steele101.com>
2020-03-03 23:33:59 -0500

    *

---
+++ exercises_javascript_basics/10_objects/3.js
@@ -0,0 +1,11 @@
+let fido = {
+  name: 'Fido',
+  species: 'Labrador Retriever',
+  color: 'brown',
+  weight: 16,
+};
+
+fido.age = 3;
+fido['favorit food'] = 'chocolate cheesecake';
+
+console.log(fido);
